710bdf8275c703ca0e3bd9227f11fa83ad425471
j-0-n-e-z <j000neeez@gmail.com>
2026-01-29 21:00:48 +0500

    update button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,18 @@
-interface ButtonProps {
-	children: React.ReactNode
+import clsx from 'clsx';
+
+import styles from './Button.module.css';
+
+type ButtonVariant = 'contained' | 'outlined';
+
+interface ButtonProps extends React.ComponentProps<'button'> {
+  children: React.ReactNode;
+  variant: ButtonVariant;
 }
 
-export const Button = ({ children }: ButtonProps) => {
-	return <button>{children}</button>
-}
+export const Button = ({ children, variant, className, ...props }: ButtonProps) => {
+  return (
+    <button className={clsx(styles.button, styles[variant], className)} type='button' {...props}>
+      {children}
+    </button>
+  );
+};
