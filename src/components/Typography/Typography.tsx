3651e5c09ccbeaff37cdf453844ceda655491b9e
j-0-n-e-z <j000neeez@gmail.com>
2026-01-29 21:01:23 +0500

    update Typography component

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -1,24 +1,30 @@
-import React from 'react'
+import clsx from 'clsx';
+import React from 'react';
 
-import styles from './Typography.module.css'
+import styles from './Typography.module.css';
 
-type TypographyTag = 'div' | 'h1' | 'h2' | 'h3' | 'h4' | 'p' | 'span'
-type TypographyVariant = 'title'
+type TypographyTag = 'div' | 'h1' | 'h2' | 'h3' | 'h4' | 'p' | 'span';
+type TypographyVariant = 'paragraph16-regular' | 'title';
 
 // exclude 'span' because property 'align' from 'span'  breaks it
 type TypographyProps<Tag extends TypographyTag> = React.ComponentProps<Exclude<Tag, 'span'>> & {
-	tag?: TypographyTag
-	variant: TypographyVariant
-	children: React.ReactNode,
-}
+  tag?: TypographyTag;
+  variant: TypographyVariant;
+  children: React.ReactNode;
+};
 
 export const Typography = <Tag extends TypographyTag = 'div'>({
-	tag = 'div',
-	variant,
-	children,
-	...props
+  tag = 'div',
+  variant,
+  children,
+  className,
+  ...props
 }: TypographyProps<Tag>) => {
-	const Component = tag
+  const Component = tag;
 
-	return <Component className={styles[variant]} {...props}>{children}</Component>
-}
+  return (
+    <Component className={clsx(styles[variant], className)} {...props}>
+      {children}
+    </Component>
+  );
+};
