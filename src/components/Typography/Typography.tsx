d797dac9d2a477bcd3284e6974811920b3234d4f
j-0-n-e-z <j000neeez@gmail.com>
2026-01-29 19:37:09 +0500

    add props drilling for Typography

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -5,18 +5,20 @@ import styles from './Typography.module.css'
 type TypographyTag = 'div' | 'h1' | 'h2' | 'h3' | 'h4' | 'p' | 'span'
 type TypographyVariant = 'title'
 
-type TypographyProps<Tag extends TypographyTag> = React.ComponentProps<Tag> & {
+// exclude 'span' because property 'align' from 'span'  breaks it
+type TypographyProps<Tag extends TypographyTag> = React.ComponentProps<Exclude<Tag, 'span'>> & {
 	tag?: TypographyTag
 	variant: TypographyVariant
-	children: React.ReactNode
+	children: React.ReactNode,
 }
 
 export const Typography = <Tag extends TypographyTag = 'div'>({
 	tag = 'div',
 	variant,
-	children
+	children,
+	...props
 }: TypographyProps<Tag>) => {
 	const Component = tag
 
-	return <Component className={styles[variant]}>{children}</Component>
+	return <Component className={styles[variant]} {...props}>{children}</Component>
 }
