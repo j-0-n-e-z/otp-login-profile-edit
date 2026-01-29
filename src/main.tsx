c36ffe9f926467709e902118a5360d7778b7d2d0
j-0-n-e-z <j000neeez@gmail.com>
2026-01-29 20:59:17 +0500

    add reset.css

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App.tsx';
 import { Provider } from './provider.tsx';
 
+import './styles/reset.css'
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
