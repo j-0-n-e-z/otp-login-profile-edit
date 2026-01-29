59c83b76e97c716b2775f3afde0d3134850e8b84
j-0-n-e-z <j000neeez@gmail.com>
2026-01-29 19:23:33 +0500

    add authview

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { AuthView } from "./modules/auth/view"
+
 export const App = () => {
-  
+  return <AuthView/>
 }
---
+++ src/modules/auth/view.tsx
@@ -0,0 +1,9 @@
+import { Typography } from '@/components';
+
+export const AuthView = () => {
+  return (
+    <Typography tag='h1' variant='title'>
+      AuthView
+    </Typography>
+  );
+};
