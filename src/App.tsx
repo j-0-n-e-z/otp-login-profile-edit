c2d4f2ad59c6c851e4457df9dc5fd46a26dfd1bf
j-0-n-e-z <j000neeez@gmail.com>
2026-02-05 20:50:06 +0500

    add conditional render for logged in

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
-import { AuthView } from "@/modules/auth/View";
+import { AuthView } from '@/modules/auth/View';
+
+import { useStore } from './utils/store';
 
 export const App = () => {
-  return <AuthView />;
+  const isLoggedIn = useStore((state) => state.isLoggedIn);
+  const user = useStore((state) => state.user);
+
+  return (
+    <>
+      {!isLoggedIn && <AuthView />}
+      {isLoggedIn && <>{user.phone}</>}
+    </>
+  );
 };
