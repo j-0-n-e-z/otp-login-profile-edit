eed5db62cbb8e27b3fa5578ba9e7830fd4e5e547
j-0-n-e-z <j000neeez@gmail.com>
2026-02-05 20:48:25 +0500

    add token to local storage

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,27 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import { App } from './App.tsx';
-import { Provider } from './provider.tsx';
+import { App } from './App';
+import { Provider } from './provider';
+import { getUserSession } from './utils/api/requests/getUserSession';
+import { LOCAL_STORAGE_KEYS } from './utils/constants/index';
+import { useStore } from './utils/store/index';
 
-import './styles/reset.css'
 import './index.css';
+import './styles/reset.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+const init = async () => {
+  const token = localStorage.getItem(LOCAL_STORAGE_KEYS.TOKEN);
+
+  if (token) {
+    const getUserSessionResponse = await getUserSession();
+    useStore.setState({ isLoggedIn: true, user: getUserSessionResponse.data.user });
+  }
+
+  createRoot(document.getElementById('root')!).render(
     <Provider>
       <App />
     </Provider>
-  </StrictMode>
-);
+  );
+};
+
+init();
