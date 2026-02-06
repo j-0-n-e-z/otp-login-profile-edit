fe02a18a3d3713e17cb2f0882824b38d097d4c78
j-0-n-e-z <j000neeez@gmail.com>
2026-02-06 11:34:53 +0500

    add interceptor for Authorization token

--- src/utils/api/instance.ts
+++ src/utils/api/instance.ts
@@ -1,5 +1,18 @@
 import axios from 'axios';
 
+import { useToken } from '../store';
+
 export const api = axios.create({
-  baseURL: 'http://localhost:31299/api/'
+  baseURL: 'http://localhost:31299/api/',
+  validateStatus: () => true
+});
+
+api.interceptors.request.use((config) => {
+  const { token } = useToken.getState();
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
